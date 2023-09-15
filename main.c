e456b347e956691cf71bb0f49873c3e8c080bb51
selena <salihfaty1211@gmail.com>
2023-09-15 10:24:05 +0100

    challenge 1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nom[10]= "Salih";
+char prenom[20]= "Fatima Ezzahrae";
+int age = 19;
+char sexe[5]= "F";
+char nm[12] = "0775292446";
+
+printf("Nom : %s\n",nom );
+printf("Prenom : %s\n",prenom);
+printf("Age : %d\n",age);
+printf("sexe : %s\n",sexe);
+printf("Numero de telephone : %s\n",nm);
+
+
+    return 0;
+}
